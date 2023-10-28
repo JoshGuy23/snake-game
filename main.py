7cc550d9be6f2793570b2bf4831d24927ed59af4
Joshua Hecker <jhecker2001@gmail.com>
2023-10-28 09:58:48 -0500

    Created functions for setting up the screen and creating snake segments, and added functionality for snake movement.

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from turtle import Turtle, Screen
+import time
 # Notes: Turtles are 20 pixels by 20 pixels
 
 
-def start():
+def setup_screen():
     screen = Screen()
     screen.setup(height=600, width=600)
     screen.bgcolor("black")
     screen.title("Snake")
+    screen.tracer(0)
+    return screen
 
+
+def create_segment():
+    new_segment = Turtle()
+    new_segment.shape("square")
+    new_segment.color("white")
+    new_segment.penup()
+    return new_segment
+
+
+def play_game(snake, screen):
+    playing = True
+    while playing:
+        screen.update()
+        time.sleep(0.1)
+        for seg_num in range(len(snake) - 1, 0, -1):
+            new_x = snake[seg_num - 1].xcor()
+            new_y = snake[seg_num - 1].ycor()
+            snake[seg_num].goto(new_x, new_y)
+        snake[0].forward(20)
+
+
+def start():
+    screen = setup_screen()
     snake = []
     x_pos = 0
     for _ in range(3):
-        new_segment = Turtle()
-        new_segment.shape("square")
-        new_segment.color("white")
-        new_segment.penup()
+        new_segment = create_segment()
         new_segment.setposition(y=0, x=x_pos)
         x_pos -= 20
         snake.append(new_segment)
+    play_game(snake=snake, screen=screen)
+
     screen.exitonclick()
 
 
